71148d6d718813246ad755734e280e285e3c4edd
hongsh429 <shh0429@kakao.com>
2024-08-05 23:25:21 +0900

    api 개발 고급
    - XxxToOne 성능 최적화
    - dto로 변경. 그렇지만, 결국에 lazy모드 때문에 쿼리가 더 나간다

--- src/main/java/lecture/jpa2/Jpa2Application.java
+++ src/main/java/lecture/jpa2/Jpa2Application.java
@@ -3,7 +3,6 @@ package lecture.jpa2;
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Jpa2Application {
@@ -13,7 +12,7 @@ public class Jpa2Application {
   }
 
   // 기본적으로 초기화 된 프록시 객체만 노출, 초기화 되지 않은 프록시 객체는 노출 안함
-  @Bean
+//  @Bean
   public Hibernate5JakartaModule hibernate5JakartaModule() {
     Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
     // 강제 지연 로딩 설정
--- src/main/java/lecture/jpa2/api/OrderSimpleApiController.java
+++ src/main/java/lecture/jpa2/api/OrderSimpleApiController.java
@@ -1,14 +1,19 @@
 package lecture.jpa2.api;
 
 
+import lecture.jpa2.domain.Address;
 import lecture.jpa2.domain.Order;
+import lecture.jpa2.domain.OrderStatus;
 import lecture.jpa2.repository.OrderRepository;
 import lecture.jpa2.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Order
@@ -21,6 +26,7 @@ import java.util.List;
 public class OrderSimpleApiController {
 
   private final OrderRepository orderRepository;
+
   /**
    * @V1. 엔티티 직접 노출
    * @- Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
@@ -29,9 +35,38 @@ public class OrderSimpleApiController {
   public List<Order> orderV1() {
     List<Order> all = orderRepository.findAllByString(new OrderSearch());
     for (Order order : all) {
-      order.getMember().getName(); //
-      order.getDelivery().getAddress();
+      order.getMember().getName(); // Lazy로딩 강제
+      order.getDelivery().getAddress(); // Lazy로딩 강제
     }
     return all;
+  }
+
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDTO> orderV2() {
+    List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+    List<SimpleOrderDTO> result = orders.stream()
+            .map(SimpleOrderDTO::new)
+            .collect(Collectors.toList());
+
+    return result;
+  }
+
+
+  @Data
+  public static class SimpleOrderDTO {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+
+    public SimpleOrderDTO(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName(); // Lazy 초기화
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress(); // Lazy 초기화
+    }
   }
 }
